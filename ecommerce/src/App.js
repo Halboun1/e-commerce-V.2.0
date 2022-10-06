96507ba0b922857968f97056f9f07096ffaccdf8
Boun13a <halbounicd@gmail.com>
2022-10-06 16:10:24 -0400

    initializing the react app

---
+++ ecommerce/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">OShop</a>
+      </header>
+      <main>
+        List of products
+      </main>
+    </div>
+  );
+}
+
+export default App;
