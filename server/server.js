5c1cfe201058b46af695050fb676edc58f79e5ef
Boun13a <halbounicd@gmail.com>
2022-10-11 22:48:00 -0400

    fixed the adding item to cart bug

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.get('/api/products', (req,res) =>{
 });
 
 app.get('/api/products/slug/:slug', (req,res) =>{
-    const product = data.products.find(x => x.slug === req.params.slug);
+    const product = data.products.find((x) => x.slug === req.params.slug);
     if(product)
     {
         res.send(product);
@@ -19,17 +19,14 @@ app.get('/api/products/slug/:slug', (req,res) =>{
     res.send(data.products);
 });
 
-app.get('/api/products/slug/:id', (req,res) =>{
-    const product = data.products.find(x => x._id === req.params.id);
-    if(product)
-    {
-        res.send(product);
+app.get('/api/products/:id', (req, res) => {
+    const product = data.products.find((x) => x._id === req.params.id);
+    if (product) {
+      res.send(product);
+    } else {
+      res.status(404).send({ message: 'Product Not Found' });
     }
-    else{
-        res.status(404).send({message: 'Product Not Found'});
-    }
-    res.send(data.products);
-});
+  });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
