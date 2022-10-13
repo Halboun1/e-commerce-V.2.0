90b535a8c04c9797e9df66ecd22b2d47b2a66095
Boun13a <halbounicd@gmail.com>
2022-10-12 20:20:46 -0400

    fixing few bugs related to cart items

--- ecommerce/src/screens/CartScreen.js
+++ ecommerce/src/screens/CartScreen.js
@@ -72,55 +72,55 @@ export default function CartScreen() {
                                                         <Link to={`/product/${item.slug}`}>{item.name}</Link>
                                                 </Col>
                                                 <Col md={3}>
-                                                    <Button variant="light"
-                                                        onClick={()=> updateCartHandler(item, item.quantity - 1)}
-                                                        disabled={item.quantity === 1}>
-                                                        <i className="fas fa-minus-circle"></i>
-                                                    </Button>{' '}
-                                                    <span>{item.quantity}</span>{' '}
-                                                    <Button variant="light"
-                                                        onClick={()=> updateCartHandler(item, item.quantity + 1)}
-                                                        disabled={item.quantity === item.countInStock}>
-                                                        <i className="fas fa-plus-circle"></i>
-                                                    </Button>
-                                                </Col>
-                                                <Col md={3}>${item.price}</Col>
-                                                <Col md={2}>
-                                                    <Button onClick={()=> removeItemHandler(item)} variant="light">
-                                                        <i className="fas fa-trash"></i>
-                                                    </Button>
-                                                </Col>
-                                            </Row>
-                                        </ListGroup.Item>
-                                    ))
-                                }
-                            </ListGroup>
-                        )
-                    }
-                </Col>
-                <Col md={4}>
-                    <Card>
-                        <Card.Body>
-                            <ListGroup variant="flush">
-                                <ListGroup.Item>
-                                    <h3>
-                                        Subtotal ({cartItems.reduce((a,c)=> a + c.quantity,0)} {' '}
-                                        items) : $ 
-                                        {cartItems.reduce((a,c) => a + c.price * c.quantity,0)}
-                                    </h3>
-                                </ListGroup.Item>
-                                <ListGroup.Item>
-                                    <div className="d-grid">
-                                        <Button type="button" variant="primary" onClick={checkoutHandler} disabled={cartItems.length === 0}>
-                                            Proceed to Checkout
-                                        </Button>
-                                    </div>
-                                </ListGroup.Item>
-                            </ListGroup>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </div>
-    );
+                                                <Button variant="light"
+onClick={()=> updateCartHandler(item, item.quantity - 1)}
+disabled={item.quantity === 1}>
+<i className="fas fa-minus-circle"></i>
+</Button>{' '}
+<span>{item.quantity}</span>{' '}
+<Button variant="light"
+onClick={()=> updateCartHandler(item, item.quantity + 1)}
+disabled={item.quantity === item.countInStock}>
+<i className="fas fa-plus-circle"></i>
+</Button>
+</Col>
+<Col md={3}>${item.price}</Col>
+<Col md={2}>
+<Button onClick={()=> removeItemHandler(item)} variant="light">
+<i className="fas fa-trash"></i>
+</Button>
+</Col>
+</Row>
+</ListGroup.Item>
+))
+}
+</ListGroup>
+)
+}
+</Col>
+<Col md={4}>
+<Card>
+<Card.Body>
+<ListGroup variant="flush">
+<ListGroup.Item>
+<h3>
+Subtotal ({cartItems.reduce((a,c)=> a + c.quantity,0)} {' '}
+items) : $ 
+{cartItems.reduce((a,c) => a + c.price * c.quantity,0)}
+</h3>
+</ListGroup.Item>
+<ListGroup.Item>
+<div className="d-grid">
+<Button type="button" variant="primary" onClick={checkoutHandler} disabled={cartItems.length === 0}>
+Proceed to Checkout
+</Button>
+</div>
+</ListGroup.Item>
+</ListGroup>
+</Card.Body>
+</Card>
+</Col>
+</Row>
+</div>
+);
 }
