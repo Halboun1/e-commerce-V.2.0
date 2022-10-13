3391596430309b42432db0bfd6a118c70c1483e3
Boun13a <halbounicd@gmail.com>
2022-10-13 18:28:53 -0400

    adding a connection to mangodb atlas

--- ecommerce/src/screens/SigninScreen.js
+++ ecommerce/src/screens/SigninScreen.js
@@ -20,23 +20,25 @@ export default function SigninScreen() {
             <Helmet>
                 <title>Sign In</title>
             </Helmet>
+            <div className="signin-container">
             <h1 className="my-3">Sign In</h1>
-            <Form>
-                <Form.Group className="mb-3" controlId="email">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" required/>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="password">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" required/>
-                </Form.Group>
-                <div className="mb-3">
-                    <Button type="submit">Sign In</Button>
-                </div>
-                <div className="mb-3">New customer?{' '}
-                    <Link to={`/signup?redirect=${redirect}`}>Create new account</Link>
-                </div>
-            </Form>
+                <Form>
+                    <Form.Group className="mb-3" controlId="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" required/>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" required/>
+                    </Form.Group>
+                    <div className="mb-3">
+                        <Button type="submit">Sign In</Button>
+                    </div>
+                    <div className="mb-3">New customer?{' '}
+                        <Link to={`/signup?redirect=${redirect}`}>Create new account</Link>
+                    </div>
+                </Form>
+            </div>
         </Container>
     )
 }
