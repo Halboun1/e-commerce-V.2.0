fd7abcfbf8e99aa432ece9a13f201cd31cf14b21
Boun13a <halbounicd@gmail.com>
2022-10-13 22:31:00 -0400

    fixed seed routes, also created some seeds for the database

--- server/data.js
+++ server/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+    users: [
+        {
+            name: "Mohamed",
+            email: 'mo.halbouni@gmail.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: true,
+        },
+        {
+            name: 'Ahmed',
+            email: 'ahmed@gmail.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: false,
+        },
+    ],
     products: [
         {
             name: 'Headphones PB',
