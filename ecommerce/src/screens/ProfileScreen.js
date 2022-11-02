b0be72f72098c30a9dff87090175588460f10eff
Boun13a <halbounicd@gmail.com>
2022-11-02 18:37:18 -0400

    user can successfully update his profile

--- ecommerce/src/screens/ProfileScreen.js
+++ ecommerce/src/screens/ProfileScreen.js
@@ -1,16 +1,72 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useReducer, useState } from 'react';
 import { Store } from '../Store';
 import { Helmet } from 'react-helmet-async';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { toast } from 'react-toastify';
+import { getError } from '../utils';
+import axios from 'axios';
+
+
+
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case 'UPDATE_REQUEST':
+            return {...state, loadingUpdate: true};
+
+        case 'UPDATE_SUCCESS':
+            return {...state, loadingUpdate: false};
+
+        case 'UPDATE_FAIL':
+            return {...state, loadingUpdate: false};
+
+        default:
+            return state;
+    }
+}
 
 export default function ProfileScreen() {
     const { state, dispatch: ctxDispatch } = useContext(Store);
     const { userInfo } = state;
 
-    const [name, setName] = useState(userInfo.name);
-    const [email, setEmail] = useState(userInfo.email);
+    const [ name, setName] = useState(userInfo.name);
+    const [ email, setEmail] = useState(userInfo.email);
+    const [ password, setPassword] = useState('');
+    const [ confirmPassword, setConfirmPassword] = useState('');
 
-    const submitHandler = async ()=>{};
+    const [{loadingUpdate}, dispatch] = useReducer(reducer, {
+        loadingUpdate: false,
+    });
+
+    const submitHandler = async (e)=>{
+        e.preventDefault();
+        try {
+            const { data } = await axios.put(
+                '/api/users/profile',
+                {
+                    name,
+                    email,
+                    password,
+                },
+                {
+                    headers: { Authorization: `Bearer ${userInfo.token} `},
+                }
+            );
+            dispatch({
+                type: 'UPDATE_SUCCESS',
+            });
+            ctxDispatch({type: 'USER_SIGNIN', payload: data});
+            localStorage.setItem('userInfo', JSON.stringify(data));
+            toast.success('Your profile has been updated successfully');
+            
+        } catch (err) {
+            dispatch({
+                type: 'FETCH_FAIL',
+            });
+            toast.error(getError(err));
+        }
+    };
 
   return (
     <div className="container small-container">
@@ -26,6 +82,7 @@ export default function ProfileScreen() {
                     onChange={(e) => setName(e.target.value)}
                     required/>
             </Form.Group>
+
             <Form.Group className="mb-3" controlId="name">
                 <Form.Label>Email</Form.Label>
                 <Form.Control
@@ -34,6 +91,25 @@ export default function ProfileScreen() {
                     onChange={(e) => setEmail(e.target.value)}
                     required/>
             </Form.Group>
+
+            <Form.Group className="mb-3" controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control 
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}/>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="password">
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control
+                    type="password"
+                    onChange={(e) => setConfirmPassword(e.target.value)}/>
+            </Form.Group>
+
+            <div className="mb-3">
+                <Button type="submit">Update</Button>
+            </div>
+
         </form>
     </div>
   )
