6c2c3dca9a72b8807601df7123ff1a1de81bc59a
Boun13a <halbounicd@gmail.com>
2022-11-03 16:37:24 -0400

    Publish-To-Heroku

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/ecommerce/build')));
 app.get('*',(req,res) =>
   res.sendFile(path.join(__dirname, '/ecommerce/build/index.html'))
-);
+);  
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
